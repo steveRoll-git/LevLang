e762c8c82bb5864773961466bce596e907860ab1
Ron Tseytlin <ronts390@gmail.com>
2021-09-10 21:31:40 +0300

    Add project files.

---
+++ LevCompiler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LevCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
